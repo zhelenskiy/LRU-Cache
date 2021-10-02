fdacf93e28da73c7d521c65485a80afc938285b2
zhelenskiy <zhelenskiy2000@yandex.ru>
2021-10-02 14:09:18 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+}
+
+group = "me.zhelenskiy"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("com.google.truth:truth:1.1.3")
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "LRU-Cache"
+
