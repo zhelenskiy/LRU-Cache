c8262237cbb5c44cc00c1c25f933b7c42423f32a
zhelenskiy <zhelenskiy2000@yandex.ru>
2021-10-02 15:32:51 +0300

    Hiding LinkedListNode.kt internal state

--- src/main/kotlin/LinkedListNode.kt
+++ src/main/kotlin/LinkedListNode.kt
@@ -1,8 +1,34 @@
-internal class LinkedListNode<E>(
+class LinkedListNode<E> private constructor(
     val value: E,
-    var previous: LinkedListNode<E>? = null,
-    var next: LinkedListNode<E>? = null,
-): Iterable<E> {
+    previous: LinkedListNode<E>?,
+    next: LinkedListNode<E>?,
+) : Iterable<E> {
+    constructor(value: E): this(value, null, null)
+
+    var previous: LinkedListNode<E>? = previous
+        private set
+    var next: LinkedListNode<E>? = next
+        private set
+
+    private fun merge(previous: LinkedListNode<E>?, newValue: E, next: LinkedListNode<E>?): LinkedListNode<E> {
+        validatePair(previous, next)
+        val newNode = LinkedListNode(newValue, previous = previous, next = next)
+        previous?.next = newNode
+        next?.previous = newNode
+        validatePair(previous, newNode)
+        validatePair(newNode, next)
+        return newNode
+    }
+
+    private fun validatePair(previous: LinkedListNode<E>?, next: LinkedListNode<E>?) {
+        require(previous != next)
+        require(previous == null || previous.next == next)
+        require(next == null || next.previous == previous)
+    }
+
+    fun addBefore(newValue: E): LinkedListNode<E> = merge(previous, newValue, this)
+
+    fun addAfter(newValue: E): LinkedListNode<E> = merge(this, newValue, next)
 
     override fun iterator(): Iterator<E> = iterator {
         var current = this@LinkedListNode
--- src/test/kotlin/LinkedListNodeTests.kt
+++ src/test/kotlin/LinkedListNodeTests.kt
@@ -3,12 +3,10 @@ import org.junit.Test
 
 class LinkedListNodeTests {
     private fun makeList(size: Int): LinkedListNode<Int>? {
-        val nodes = Array(size) { LinkedListNode(it) }
-        for ((prev, next) in nodes.indices zip nodes.indices.drop(1)) {
-            nodes[prev].next = nodes[next]
-            nodes[next].previous = nodes[prev]
-        }
-        return if (size > 0) nodes[0] else null
+        if (size == 0) return null
+        val first = LinkedListNode(0)
+        generateSequence(first) { it.addAfter(it.value + 1) }.take(size).toList()
+        return first
     }
 
     @Test
